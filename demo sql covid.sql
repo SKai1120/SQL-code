592b69186e3a33b7ca526879c47c7822000f8848
SKai1120 <91954310+SKai1120@users.noreply.github.com>
2022-01-20 15:29:49 +0900

    Add files via upload

--- demo sql covid.sql
+++ demo sql covid.sql
@@ -1,3 +1,6 @@
+
+--check for valid data
+
 --select *
 --from SQLtest..CovidDeath
 --order by 3,4
@@ -6,15 +9,16 @@
 --from SQLtest..CovidVaccin
 --order by 3,4
 
---Select use data
+--Select use data (BY WHICH COLUMN, FROM WHICH TABLE, IN WHICH ORDER)
 Select Location, date, total_cases, new_cases, total_deaths, population
-from SQLtest..CovidDeath
+from SQLtest..CovidDeath 
 order by location, date
 
 --Total Cases vs Total Deaths
---Death Percentage 
+--Death Percentage ((total_deaths/total_cases)*100 as DeathPercent) CREATE A DEATHPERCENT COLUMN WITH THE EXISTING COLUMN IN THE TABLE
 Select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercent
 from SQLtest..CovidDeath
+--SPECIFY WHICH COUNTRY TO LOOKUP OR GLOBAL
 where location like '%viet%'
 order by Location, date
 
@@ -23,17 +27,25 @@ Select location, date, total_cases, population, (total_cases/population)*100 as 
 from SQLtest..CovidDeath
 order by location, date
 
---Country with highest InfectRate compared to Population
-Select Location, population, MAX(total_cases) as HighestInfectCount, MAX((total_cases/population))*100 as PerPopulationInfect
+--Country w highest InfectRate compared to Population
+Select Location, population, 
+--TOTAL NUMBER OF CASES SELECT MAX CUZ DATE INVOLVE
+MAX(total_cases) as HighestInfectCount, 
+--PERCENTAGE OF POPULATION INFECTED PER COUNTRY
+MAX((total_cases/population))*100 as PerPopulationInfect
 from SQLtest..CovidDeath
 group by Location, Population 
+--DECREASING MANNER
 order by PerPopulationInfect desc
 
-----Countries w Highest Death Count per Population
-Select Location, MAX(cast(total_deaths as int)) as TotalDeathCount
+----Countries w Highest Death Count 
+Select Location, population,
+--CONVERT NVARCHAR TO INT FOR NUMERIC EQUATION
+MAX(cast(total_deaths as int)) as TotalDeathCount
 from SQLtest..CovidDeath
+--CONTINENT COLUMN EXIST IN EXCEL FILE
 where continent is not NULL
-group by Location
+group by Location, population
 order by TotalDeathCount desc
 
 --Highest Death Count per Population by Continent
@@ -44,7 +56,6 @@ group by continent
 order by TotalDeathCount desc
 
 --Global numbers
-
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, (SUM(cast(new_deaths as int))/SUM(new_cases))*100 as DeathPercent
 from SQLtest..CovidDeath
 where continent is not NULL
@@ -78,14 +89,16 @@ New_vaccinations numeric,
 TotalPplVaccinated numeric
 )
  Insert into #VaccinatedbyPopPercent
+ --NEW VACCINATION SORTED BY DATE W TOTAL VACCINATED INCREMENTS
  select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, sum(convert(numeric,vac.new_vaccinations)) over (PARTITION by dea.location order by dea.location, dea.date) as TotalPplVaccinated
+ --CAN'T PUT IN PERCENTAGE W NEWLY CREATED TotalPplVaccinated
 from SQLtest..CovidDeath dea
 join SQLtest..CovidVaccinations vac
 	on dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not NULL
 --order by 2, 3
-
+--ADD PERCENTAGE TO TABLE
 select *, (TotalPplVaccinated/population)*100
 from #VaccinatedbyPopPercent
 
